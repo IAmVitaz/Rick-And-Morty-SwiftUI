5bca04a499a099957c6577ee57c984973ab4e0d2
vit_az <vit.azarov@gmail.com>
2022-03-29 18:06:24 -0300

    Initial Commit

---
+++ Rick And Morty SwiftUI/Rick_And_Morty_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_Morty_SwiftUIApp.swift
+//  Rick And Morty SwiftUI
+//
+//  Created by Vitalii Azarov on 2022-03-29.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_Morty_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
